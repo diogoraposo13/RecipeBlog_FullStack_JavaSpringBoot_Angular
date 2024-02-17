93b052450c7141d0c4f99a2194fac0fc7f9ffab3
diogoraposo13 <diogo.raposo@live.com.pt>
2024-02-17 15:11:49 +0000

    backend initial setup

---
+++ backend/recipe-blog/src/test/java/com/diogoraposo/recipeblog/RecipeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoraposo.recipeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
